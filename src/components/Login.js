0855d81d8e742406760929799cf2a9b5c8631d69
seongck <seongk@pm.me>
2019-10-20 19:27:09 -0700

    Add Login component elements

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input, Button, FormFeedback } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 function Login() {
   return (
