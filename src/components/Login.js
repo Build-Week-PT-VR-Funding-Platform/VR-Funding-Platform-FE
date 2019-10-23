939b5528218908d255b29b37014b4d523640e436
seongck <seongk@pm.me>
2019-10-22 20:19:08 -0700

    Change Login redirect path to /dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,7 +44,7 @@ function Login({ history }) {
         setReqError({
           error: ''
         });
-        history.push('/account');
+        history.push('/dashboard');
       })
       .catch( err => {
         console.log(err)
