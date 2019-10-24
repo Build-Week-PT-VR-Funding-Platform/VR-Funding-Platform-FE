3f3443b62fbb643fc1f6455fa7f751750033cc4c
seongck <seongk@pm.me>
2019-10-23 20:04:54 -0700

    Add logged in NavLinks and logout functionality

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 import { Collapse, Navbar, NavbarToggler, Nav, NavItem, Button } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 
 const MainNav = () => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [ user, setUser ] = useContext(UserContext);
 
   const toggle = () => setIsOpen(!isOpen);
 
-  return (
+  const logOut = () => {
+    localStorage.removeItem('token');
+    setUser({
+      id: null,
+      name: '',
+      username: ''
+    });
+  };
+
+  if(!user.id) { 
+    return ( 
+      <Navbar color="light" light expand="md">
+        <NavLink to={`/`} className="navbar-brand">Dreality</NavLink>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink to={`/`} className="nav-link">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to={`/projects`} className="nav-link">Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to={`/login`} className="nav-link"><Button color="secondary">Log In</Button></NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to={`/signup`} className="nav-link"><Button color="primary">Sign Up</Button></NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    )
+  } else {
+    return (
     <Navbar color="light" light expand="md">
       <NavLink to={`/`} className="navbar-brand">Dreality</NavLink>
       <NavbarToggler onClick={toggle} />
@@ -21,15 +56,23 @@ const MainNav = () => {
             <NavLink to={`/projects`} className="nav-link">Projects</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink to={`/login`} className="nav-link"><Button color="secondary">Log In</Button></NavLink>
+            <NavLink to={`/entrepreneurs`} className="nav-link">Entrepreneurs</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink to={`/signup`} className="nav-link"><Button color="primary">Sign Up</Button></NavLink>
+            <NavLink to={`/dashboard`} className="nav-link">Dashboard</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to={`/account`} className="nav-link">Account Settings</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to={`/`} className="nav-link">
+              <Button color="secondary" type="button" onClick={logOut}>Log Out</Button>
+            </NavLink>
           </NavItem>
         </Nav>
       </Collapse>
     </Navbar>
-  )
+    )}
 }
 
 export default MainNav;
