b0275754b482ecf54c287d1f3d1995c4541d13fa
seongck <seongk@pm.me>
2019-10-23 19:26:55 -0700

    Remove unused Link import from Account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 import { Container, Form, FormGroup, FormText, Label, Input, Button, Alert, Spinner, ButtonGroup } from 'reactstrap';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
