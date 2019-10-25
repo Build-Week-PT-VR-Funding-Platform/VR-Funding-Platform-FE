219fa4c25ad4fe2b047f0d66e34e03c8ec31753e
seongck <seongk@pm.me>
2019-10-24 21:03:56 -0700

    Remove nested <h5> in <p> error and replace with .card-subheader className style

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -7,7 +7,7 @@ const CTA = props =>{
     <Card>
       <CardHeader tag="h3">Welcome!</CardHeader>
       <CardBody>
-        <CardText><h5>What are we about?</h5></CardText>
+        <CardText className="card-subheader">What are we about?</CardText>
         <CardText>We are here to provide <strong>YOU</strong> with a platform to access business training, project capital, and mentorship. 
           <br /> In case you didn't see our fancy animation on the loading screen because our website is so efficient: 
           <Spinner color="danger" />
